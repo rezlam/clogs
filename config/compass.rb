ddb440fef4dd3326b5cc7e621c5068d2077b6502
Gabriel Lamounier <grezlam@gmail.com>
2011-03-14 00:40:59 -0300

    Make compass use sass intended syntax

--- config/compass.rb
+++ config/compass.rb
@@ -9,6 +9,5 @@ sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-# Using intended syntax
-# syntax = "sass"
-# property_syntax = "new"
+preferred_syntax = :sass
+
